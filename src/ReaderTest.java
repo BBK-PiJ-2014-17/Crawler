a0f21a0bdff5713f8c88747edd207b197b2add73
BasilMason <basilmason@hotmail.com>
2015-03-31 21:43:02 +0100

    readString tests added to ReaderTest

--- src/ReaderTest.java
+++ src/ReaderTest.java
@@ -13,6 +13,12 @@ import java.net.URL;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/*
+    - does inputStream to string consume inputStream
+    - check case
+    - check multiple types of whitespace
+ */
+
 public class ReaderTest {
 
     private HTMLread reader;
@@ -41,6 +47,8 @@ public class ReaderTest {
     @Test
     public void testReadUntil() throws Exception {
 
+        // check case
+
         /*
             ** expected beginning of inputStream = '<!doctype html>'
          */
@@ -49,25 +57,24 @@ public class ReaderTest {
 
         // first char found expects true and input stream read to point
         ch1 = 't';                  // char at 6th position
-        ch2 = Character.MIN_VALUE;  // default, not relevant
-        expected = "type html>";    // first 10 characters of expected remaining inputStream
+        ch2 = Character.MIN_VALUE;  // default, not relevant in this test
+        expected = "ype html>";     // first 9 characters of expected remaining inputStream
         assertTrue(reader.readUntil(inputStream, ch1, ch2));    // readUntil expected to return true
-        remainder = inputStream.toString().substring(0, 10);    // get portion of remaining input stream
+        remainder = inputStream.toString().substring(0, 9);    // get portion of remaining input stream
         assertTrue(remainder.equals(expected));                 // compare strings
 
         // second char found expects false and input stream read to point
-        ch1 = Character.MIN_VALUE;  // default, not relevant
+        ch1 = Character.MIN_VALUE;  // default, not relevant in this test
         ch2 = 'h';                  // char at 11th position
-        expected = "html>";         // first 6 characters of expected remaining inputStream
+        expected = "tml>";          // first 5 characters of expected remaining inputStream
         assertTrue(!reader.readUntil(inputStream, ch1, ch2));   // readUntil expected to return false
-        remainder = inputStream.toString().substring(0, 6);     // get portion of remaining input stream
+        remainder = inputStream.toString().substring(0, 5);     // get portion of remaining input stream
         assertTrue(remainder.equals(expected));                 // compare strings
-
 
         // neither char found expects false and empty stream remaining
         int i;                      // expected return from empty inputStream
-        ch1 = Character.MIN_VALUE;  // default, not relevant
-        ch2 = Character.MIN_VALUE;  // default, not relevant
+        ch1 = Character.MIN_VALUE;  // default, not relevant in this test
+        ch2 = Character.MIN_VALUE;  // default, not relevant in this test
         assertTrue(!reader.readUntil(inputStream, ch1, ch2));   // readUntil expected to return false
         assertTrue((i=inputStream.read()) == -1);               // check inputStream is empty
 
@@ -78,6 +85,7 @@ public class ReaderTest {
 
         // test with various types of whitespace
 
+
         /*
             ** expected beginning of inputStream = '<!doctype html>'
          */
@@ -85,7 +93,7 @@ public class ReaderTest {
         String remainder, remainderExpected;
 
         // first non-whitespace found returned
-        ch = Character.MIN_VALUE;               // default, not relevant
+        ch = Character.MIN_VALUE;               // default, not relevant in this test
         expected = '<';                         // expected to be '<'
         remainderExpected = "!doctype html>";   // first 14 characters of expected remaining inputStream
         assertTrue(reader.skipSpace(inputStream, ch) == expected);      // skipSpace returns char equal to expected
@@ -116,6 +124,40 @@ public class ReaderTest {
 
     @Test
     public void testReadString() throws Exception {
+
+        // check case
+
+        /*
+            ** expected beginning of inputStream = '<!doctype html>'
+         */
+        char ch1, ch2;
+        String expected, remainder, remainderExpected;
+
+        // first char found expects string of chars read returned
+        ch1 = 'c';                          // char at 6th position
+        ch2 = Character.MIN_VALUE;          // default, not relevant in this test
+        expected = "<!do";                  // expected chars read
+        remainderExpected = "type html>";   // first 10 characters of remaining inputStream
+        assertTrue(reader.readString(inputStream, ch1, ch2).equals(expected));      // readString expected to return
+                                                                                    // string of read chars
+        remainder = inputStream.toString().substring(0, 10);            // get portion of remaining input stream
+        assertTrue(remainder.equals(remainderExpected));                // compare strings
+
+        // second char found expects null and input stream read to point
+        ch1 = Character.MIN_VALUE;  // default, not relevant in this test
+        ch2 = 'l';                  // char at 9th position
+        expected = null;            // expect null return
+        remainderExpected = ">";    // next character of inputStream
+        assertTrue(reader.readString(inputStream, ch1, ch2).equals(expected));      // readString expected to return null
+        remainder = String.valueOf(inputStream.read());                         // get next byte as String
+        assertTrue(remainder.equals(remainderExpected));                        // compare strings
+
+        // neither char found expects null and empty stream remaining
+        int i;                      // expected return from empty inputStream
+        ch1 = Character.MIN_VALUE;  // default, not relevant in this test
+        ch2 = Character.MIN_VALUE;  // default, not relevant in this test
+        assertTrue(reader.readString(inputStream, ch1, ch2).equals(expected));   // readUntil expected to return false
+        assertTrue((i=inputStream.read()) == -1);               // check inputStream is empty
 
     }
 }
