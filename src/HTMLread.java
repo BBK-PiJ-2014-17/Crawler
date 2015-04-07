750a86f20404632a94bedbf859c803594fa1a602
BasilMason <basilmason@hotmail.com>
2015-04-07 21:27:26 +0100

    javadoc added to HMLRead class.

--- src/HTMLread.java
+++ src/HTMLread.java
@@ -3,55 +3,69 @@ import java.io.InputStream;
 
 /**
  * Created by Basil on 28/03/2015.
+ *
+ * This is an implementation of the interface <code>Reader</code>.
+ *
+ *
  */
 public class HTMLread implements Reader {
-
-    /* I N T E R F A C E   M E T H O D S */
 
     /**
      * <code>readUntil</code>
      * {@inheritDoc}
-     * <p>
-     *
-     * </p>
      */
     @Override
     public boolean readUntil(InputStream in, char ch1, char ch2) {
 
-        int b;
+        int b;  // byte from inputStream
 
+        // IOException handling
         try {
+
+            // while there is something to read from the inputStream
             while((b=in.read()) != -1) {
 
+                // check if the byte equals the first argument
                 if ((char) b == ch1)
-                    return true;
+                    return true;    // return true
 
+                // check if the byte equals the second argument
                 if ((char) b == ch2)
-                    return false;
+                    return false;   // return false
             }
 
         } catch (IOException e) {
+            System.out.println("Error reading inputStream in HTMLRead#readUntil");
             e.printStackTrace();
         }
 
+        // if neither argument matched any of the inputStream, return false
         return false;
     }
 
+    /**
+     * <code>skipSpace</code>
+     * {@inheritDoc}
+     */
     @Override
     public char skipSpace(InputStream in, char ch) {
 
-        int b;
+        int b;  // byte from inputStream
 
+        // IOException handling
         try {
+
+            // while there is something to read from the inputStream
             while((b=in.read()) != -1) {
 
-                // test all whitespace
-
+                // test the byte for all known whitespace characters
                 if (!Character.isWhitespace((char) b)) {
+
+                    // if the byte is not whitespace and matches the argument
                     if ((char) b == ch) {
-                        return Character.MIN_VALUE;
+                        return Character.MIN_VALUE; // return char min value
                     } else {
-                        return (char) b;
+                        return (char) b;            // otherwise, return the byte read from inputStream
                     }
                 }
             }
@@ -60,34 +74,45 @@ public class HTMLread implements Reader {
             e.printStackTrace();
         }
 
+        // if no non-whitespace characters are found, return char min value
         return Character.MIN_VALUE;
 
     }
 
+    /**
+     * <code>readString</code>
+     * {@inheritDoc}
+     */
     @Override
     public String readString(InputStream in, char ch1, char ch2) {
 
-        StringBuilder sb = new StringBuilder();
-        int b;
+        StringBuilder sb = new StringBuilder(); // stringBuilder to compile return string
+        int b;  // byte from inputStream
 
+        // IOException handling
         try {
+
+            // while there is something to read from the inputStream
             while((b=in.read()) != -1) {
 
-                char c = (char) b;
+                // check if the byte equals the first argument
+                if ((char) b == ch1)
+                    return sb.toString();   // return bytes read so far as string
 
-                if (c == ch1)
-                    return sb.toString();
+                // check if the byte equals the second argument
+                if ((char) b == ch2)
+                    return null;            // return null
 
-                if (c == ch2)
-                    return null;
+                sb.append((char) b);        // append byte to stringBuilder for return
 
-                sb.append(c);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // if neither argument found, return null
         return null;
     }
+
 }
